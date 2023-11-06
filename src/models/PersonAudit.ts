833c00af50eb4d8e1986333a1949b7a84abc02d5
Leonardo Lopes <leomibking@gmail.com>
2023-11-06 17:09:22 -0300

    fix: fix lint

--- src/models/PersonAudit.ts
+++ src/models/PersonAudit.ts
@@ -1,13 +1,16 @@
-import { AuditingAction, AuditingEntity, AuditingEntityDefaultColumns } from 'typeorm-auditing';
+import {
+  AuditingAction,
+  AuditingEntity,
+  AuditingEntityDefaultColumns,
+} from 'typeorm-auditing';
 import { Person } from './Person';
-import { Column } from 'typeorm';
 
 @AuditingEntity(Person)
 export class PersonAudit
   extends Person
-  implements AuditingEntityDefaultColumns {
+  implements AuditingEntityDefaultColumns
+{
   readonly _seq: number;
   readonly _action: AuditingAction;
-  @Column({ name: '_modified_at' })
   readonly _modifiedAt: Date;
 }
